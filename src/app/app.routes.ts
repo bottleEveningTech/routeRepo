48dc6ffe66e03048967a2a6e05c631bc0ab65462
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-02-07 16:44:02 +0530

    empty route component load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 
-export const routes: Routes = [{
+export const routes: Routes = [
+    {
+        path: '', //your domain localhost:4200/
+        component: NoTaskComponent
+    },
+    {
         path: 'tasks',
         component: TasksComponent//your-domain-tasks
-    }]
+    }
+]
